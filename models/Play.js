f4f2572a5bf221e41e71e4793e0a52ff25e9d59f
amzick <aaron.zick@gmail.com>
2019-11-10 10:22:56 -0500

    Define Play Schema

---
+++ models/Play.js
@@ -0,0 +1,66 @@
+/*
+  - title (becomes url)
+  - playwright
+  - show location
+  - show date (will determine year which is part of url)
+  - about (rem enters whatever)
+  - images (array of private imgur links)
+  - type: plays / youth / dramaturg
+  - press (reviews): array of objects with links with optional quote
+
+  USING OBJECTS IN MONGOOSE
+  https://stackoverflow.com/questions/42019679/object-type-in-mongoose
+*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create schema
+const PlaySchema = new Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  playwright: {
+    type: String,
+    require: true
+  },
+  location: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date,
+    require: true
+  },
+  about: {
+    type: String,
+    require: true
+  },
+  images: {
+    type: [String],
+    require: true
+  },
+  types: {
+    isPlay: {
+      type: Boolean,
+      require: true,
+      default: true
+    },
+    isWriting: {
+      type: Boolean,
+      require: true,
+      default: false
+    },
+    isYouth: {
+      type: Boolean,
+      require: true,
+      default: false
+    }
+  },
+  press: [{
+    url: String,
+    quote: String
+  }]
+});
+
+module.exports = Play = mongoose.model('plays', PlaySchema);
