efbfc0a60af99544faeab5aa7e00aace929cb912
amzick <aaron.zick@gmail.com>
2019-12-08 17:42:04 -0500

    Init Heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const plays = require('./routes/api/plays');
@@ -41,6 +42,16 @@ require('./config/passport')(passport);
 // Routes
 app.use('/api/users', users);
 app.use('/api/plays', plays);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
